fc5af4ef3085b81e4a9c2ecf8591e141854af286
Salman Mahmud <smahmud295@gmail.com>
2024-11-20 00:22:13 +0600

    push

--- components/IndividualNavItem.jsx
+++ components/IndividualNavItem.jsx
@@ -1,15 +1,19 @@
-// import Link from "next/link";
+import Link from "next/link";
 
-// export default function IndividualNavItem  ({children, path, pathname}) {
-//     const slug = pathname.split("/");
-//     return(
-//         <>
-//             <Link
-//                 href={path}
-//                 className={`flex items-center gap-3 rounded-lg py-2 text-14 ${pathname === path || pathname === path+'/'+slug[2] ? 'pl-3 bg-primary-500  text-secondary-900' : 'text-secondary-500 '} transition-all`}
-//             >
-//                 {children}
-//             </Link>
-//         </>
-//     );
-// }
+export default function IndividualNavItem({ children, path, pathname }) {
+  const slug = pathname.split("/");
+  return (
+    <>
+      <Link
+        href={path}
+        className={`flex items-center gap-3 rounded-lg py-2 text-14 ${
+          pathname === path || pathname === path + "/" + slug[2]
+            ? "pl-3 bg-primary-500  text-secondary-900"
+            : "text-secondary-500 "
+        } transition-all`}
+      >
+        {children}
+      </Link>
+    </>
+  );
+}
